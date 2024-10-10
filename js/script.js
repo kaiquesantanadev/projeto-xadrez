3bc408a376d75d16eef6adc751b978392a42aa9e
Kaique Vieira <kaiquevieira329@gmail.com>
2024-10-10 17:23:13 -0300

    wip: arrastando peça ate diferentes casas (Sem verificacoes e com bugs)

--- js/script.js
+++ js/script.js
@@ -18,8 +18,14 @@ function criarTabuleiro() {
         const quadrado = document.createElement('div')
         quadrado.setAttribute('quadrado-id', i)
         quadrado.innerHTML = peca
+
+        // verifica se o quadrado tem uma peça, se tiver deixa draggable
+        quadrado.firstChild && quadrado.firstChild.setAttribute('draggable', true) 
+
         quadrado.classList.add('quadrado')
         const linha = Math.floor((63 - i) / 8) + 1
+
+        // renderiza a linha em malha
         if (linha % 2 === 0) {
             quadrado.classList.add(i % 2 === 0 ? 'claro' : 'escuro')
         } else {
@@ -27,6 +33,7 @@ function criarTabuleiro() {
         }
         tabuleiro.append(quadrado)
 
+        // coloca cores pras peças
         if (i <= 15) {
             quadrado.firstChild.firstChild.classList.add('pecas-escuras')
         } else if (i >= 48 ) {
@@ -40,3 +47,31 @@ function criarTabuleiro() {
 }
 
 criarTabuleiro()
+
+const allQuadrados = document.querySelectorAll('#gameboard .quadrado')
+
+allQuadrados.forEach((quadrado) => {
+    quadrado.addEventListener('dragstart', dragStart)
+    quadrado.addEventListener('dragover', dragOver)
+    quadrado.addEventListener('drop', dragDrop)
+})
+
+let posicaoInicialId;
+let draggedElement
+
+function dragStart(e) {
+    posicaoInicialId = e.target.parentNode.getAttribute('quadrado-id')
+    draggedElement = e.target
+}
+
+function dragOver(e) {
+    e.preventDefault() 
+}
+
+function dragDrop(e) {
+    e.stopPropagation()
+    e.target.parentNode.append(draggedElement)
+    e.target.remove()
+    
+    // pegando a casa e dando append nela a peça draggada
+}
